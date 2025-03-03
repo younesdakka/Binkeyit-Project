b886d3e05fa485c055ca0ef2a9c8e7f8ba972739
younesdakka <younesdakka78@gmail.com>
2025-03-03 15:46:02 +0200

    format the route files

--- server/router/cart.route.js
+++ server/router/cart.route.js
@@ -1,12 +1,17 @@
 const { Router } = require("express");
 const auth = require("../middleware/auth.js");
-const { addToCartItemController, deleteCartItemQtyController, getCartItemController, updateCartItemQtyController } = require("../controllers/cart.controller.js");
+const {
+  addToCartItemController,
+  deleteCartItemQtyController,
+  getCartItemController,
+  updateCartItemQtyController,
+} = require("../controllers/cart.controller.js");
 
-const cartRouter = Router()
+const cartRouter = Router();
 
-cartRouter.post('/create',auth,addToCartItemController)
-cartRouter.get("/get",auth,getCartItemController)
-cartRouter.put('/update-qty',auth,updateCartItemQtyController)
-cartRouter.delete('/delete-cart-item',auth,deleteCartItemQtyController)
+cartRouter.post("/create", auth, addToCartItemController);
+cartRouter.get("/get", auth, getCartItemController);
+cartRouter.put("/update-qty", auth, updateCartItemQtyController);
+cartRouter.delete("/delete-cart-item", auth, deleteCartItemQtyController);
 
-module.exports= cartRouter
+module.exports = cartRouter;
